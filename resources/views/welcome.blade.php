2f2ef7244eb89596847c4b680ccd7f389f83f5f3
asfuz <ubuntusamandar@gmail.com>
2024-03-06 12:09:11 +0500

    permission

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,8 +23,6 @@
     </style>
     <!-- Add this in your HTML file, preferably in the <head> section -->
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-
-
 </head>
 
 <body class="antialiased" >
